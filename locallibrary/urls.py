e5f619598afbc6365e94b6fa1d5d694f38885066
jhanaveenk <jhanaveen255@gmail.com>
2022-12-14 19:15:51 +0530

    Skelton structure get created

---
+++ locallibrary/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/')),
+]
+# +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
